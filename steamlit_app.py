1d9ab643ee6ebc3f4fce1d4a1f17735fd9d6e84f
hue-log <115683013+hue-log@users.noreply.github.com>
2022-10-13 10:10:56 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -2,7 +2,11 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & Blueberry Oatmeal')
-streamlit.text('kale, Spinach & Rocket Smoothie')
-streamlit.text('hand-goi led Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 hand-boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Tost')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
