1aa45c92b5c3d0abb906fb0d64d8fe83bf6d02cc
hue-log <115683013+hue-log@users.noreply.github.com>
2022-10-13 10:04:08 +0530

    Update steamlit_app.py
    Fixed typo

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+streamlit.head('omega 3 & Blueberry Oatmeal')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+streamlit.text('hand-goi led Free-Range Egg')
