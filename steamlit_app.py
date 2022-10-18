c4710595b8f3946cf5a61605f5cb224fb24a2944
hue-log <115683013+hue-log@users.noreply.github.com>
2022-10-18 17:39:19 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -27,22 +27,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-expect URLError as e:
-  streamlit.error()
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?' )
-if not fruit_choice:
-  
+  fruit_choice = streamlit.text_input("What fruit would you like information about?")
+  if not fruit_choice
   streamlit.error("Please select the fruit to get information")
-
 else:
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+  fruityvice_response = requests.get( "https://fruityvice.com/api/fruit/" + fruit _ choice)
+  fruityvice_normalized= pd.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
 
+expect URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 #import snowflake.connector
