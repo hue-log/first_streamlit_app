49d206bbd152bab45ff2060814c4088ee7272ca1
hue-log <115683013+hue-log@users.noreply.github.com>
2022-10-18 16:41:12 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -41,9 +41,9 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list.fruit_load_list")
-my_data_rows = my_cur.fetchall()
+my_cur.execute("select * from fruit_load_list")
+my_data_row = my_cur.fetchone()
 streamlit.header("Fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+streamlit.dataframe(my_data_row)
 
 
