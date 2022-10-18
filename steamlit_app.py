8f9f12ac47c6fe0a539b0d1ad878c5a3a9757d69
hue-log <115683013+hue-log@users.noreply.github.com>
2022-10-18 16:58:56 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -46,4 +46,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+# allow end users to add fruits 
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write("The user entered ", add_my_fruit)
+streamlit.text_input("thanks for adding " + "add_my_fruit")
