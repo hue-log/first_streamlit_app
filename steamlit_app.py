753e276a400a7a0a67cd41b6d730dfb2e931d2cd
hue-log <115683013+hue-log@users.noreply.github.com>
2022-10-19 09:54:12 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -51,12 +51,12 @@ except URLError as e:
 streamlit.header("The fruit load list contains:")
 #snowflake related functions
 def get_fruit_load_list():
-      with my_cnx.cursor() as  my_cur:
-            my_cur.execute("select * from  fruit_load_list")
-            return my_cur.fetchall()
+    with my_cnx.cursor() as  my_cur:
+         my_cur.execute("select * from  fruit_load_list")
+         return my_cur.fetchall()
 
 # Add a button to load the fruit
-if strealit.button('Get fruit load list'):
+if streamlit.button('Get fruit load list'):
 #import snowflake.connector
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       my_data_rows = get_fruit_load_list()
