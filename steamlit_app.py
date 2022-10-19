bc314a0030d056f66aa491fe3e418c96ef4d54fb
hue-log <115683013+hue-log@users.noreply.github.com>
2022-10-19 10:45:00 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -65,11 +65,11 @@ if streamlit.button('Get fruit load list'):
 # allow end users to add fruits 
 def insert_row_snowflake(new_fruit):
     with  my_cnx.cursor() as my_cur:
-          my_cur.execute("insert into fruit_load_list values ('from streamlit' ) ")
-          return "Thanks for adding"+ new_fruit
+          my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+          return "Thanks for adding" + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-if streamlit.button("Add a Fruit to the list"):
+if streamlit.button('Add a Fruit to the list'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       back_from_function = insert_row_snowflake(add_my_fruit)
       streamlit.text(back_from_function)
